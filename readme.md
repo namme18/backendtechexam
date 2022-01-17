ff8dd3d199f0b53921077baef1e08faa21a83ee0
emman <nammeselarom@mail.com>
2022-01-17 15:20:19 +0800

    add readme.md

---
+++ readme.md
@@ -0,0 +1,9 @@
+npm run dev = to start the nodemon on development
+
+npm run build = to start build the backend
+
+npm run start = to start the build backend
+
+RootDirectory = './src'
+
+output directory = './server
--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const router = express.Router();
-const { addMessage } = require('../controllers/user');
-const jsonValidator = require('../Middleware/jsonValidator');
-router.post('/addmessage', jsonValidator, addMessage);
-//can add all user routes here...
-module.exports = router;
